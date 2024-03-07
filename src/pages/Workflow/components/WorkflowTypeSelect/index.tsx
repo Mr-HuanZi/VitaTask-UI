5f269b1e1845312ca3b4babaf48c6e61d42b3295
MrHuan <huanzi20100702@hotmail.com>
2024-03-07 16:16:05 +0800

    feat(workflow): 新增发起工作流弹窗页面

---
+++ src/pages/Workflow/components/WorkflowTypeSelect/index.tsx
@@ -0,0 +1,49 @@
+import React, {useEffect} from "react";
+import {WorkflowTypeList} from "@/services/workflow/api";
+import {codeOk} from "@/units";
+import {CheckCard} from '@ant-design/pro-card';
+
+interface WorkflowTypeSelectPropsI {
+  /** 选中的值 */
+  value?: any;
+  /** 选中的值 */
+  onChange?: (value: any) => void;
+}
+
+const WorkflowTypeSelect: React.FC<WorkflowTypeSelectPropsI> = ({value, onChange}) => {
+  const [typeList, setTypeList] = React.useState<any>([]);
+
+  useEffect(() => {
+    console.log("WorkflowTypeSelect");
+    WorkflowTypeList({
+      page: 1,
+      pageSize: 9999,
+    }).then((result) => {
+      if (codeOk(result.code)) {
+        setTypeList(result.data?.items ?? []);
+      }
+    })
+  }, []);
+
+  return (
+    <CheckCard.Group
+      onChange={(v: any) => {
+        if (onChange) {
+          onChange(v);
+        }
+      }}
+      defaultValue={value}
+    >
+      {typeList && typeList.map((item: any) => (
+        <CheckCard
+          key={item.only_name}
+          title={item.name}
+          description={item?.illustrate ?? '-'}
+          value={item.only_name}
+        />
+      ))}
+    </CheckCard.Group>
+  )
+}
+
+export default WorkflowTypeSelect;
--- src/pages/Workflow/index.tsx
+++ src/pages/Workflow/index.tsx
@@ -1,13 +1,52 @@
 import React from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { WorkflowList } from '@/services/workflow/api';
+import {WorkflowList} from '@/services/workflow/api';
 import TableList from "@/pages/Workflow/components/TableList";
+import WorkflowTypeSelect from "@/pages/Workflow/components/WorkflowTypeSelect";
+import {Button, message} from "antd";
+import {SendOutlined} from "@ant-design/icons";
+import {ModalForm} from "@ant-design/pro-form";
+import {history} from "umi";
 
 const Workflow: React.FC = () => {
+  const [selectedValue, setSelectedValue] = React.useState<any>();
+
+  const WorkflowTypeSelectToolBar = (
+    <ModalForm
+      autoFocusFirstInput
+      layout="horizontal"
+      title="发起工作流"
+      modalProps={{
+        destroyOnClose: true,
+      }}
+      trigger={
+        <Button type="primary">
+          <SendOutlined/>
+          发起工作流
+        </Button>
+      }
+      onFinish={async () => {
+        if (selectedValue) {
+          history.push(`/workflow/initiate/${selectedValue}`);
+        } else {
+          message.error('请选择工作流类型');
+        }
+      }}
+    >
+      <WorkflowTypeSelect
+        onChange={v => {
+          console.log(v);
+          setSelectedValue(v);
+        }}
+      />
+    </ModalForm>
+  )
+
   return (
     <PageContainer>
       <TableList
         requestFn={WorkflowList}
+        toolBarRender={[WorkflowTypeSelectToolBar]}
       />
     </PageContainer>
   );
