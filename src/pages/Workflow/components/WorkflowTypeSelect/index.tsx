5afe60690942d5f41bb2c211d566dcfe7aec1462
MrHuan <huanzi20100702@hotmail.com>
2025-05-26 23:10:47 +0800

    perf[工作流]: 优化发起工作流卡片选择器

--- src/pages/Workflow/components/WorkflowTypeSelect/index.tsx
+++ src/pages/Workflow/components/WorkflowTypeSelect/index.tsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, {useMemo} from "react";
 import {WorkflowTypeList} from "@/services/workflow/api";
 import {CheckCard} from '@ant-design/pro-components';
 import {useRequest} from "ahooks";
+import {Tooltip} from "antd";
+import stringWidth from "string-width";
 
 interface WorkflowTypeSelectPropsI {
   /** 选中的值 */
   value?: any;
   /** 选中的值 */
   onChange?: (value: any) => void;
+}
+
+const CardTooltip: React.FC<{text?: string}> = ({text}) => {
+  const strW = useMemo(() => {
+    if (!text)
+      return 0;
+
+    return stringWidth(text);
+  }, [text]);
+
+  return useMemo(() => {
+    if (strW <= 0)
+      return (<>-</>);
+
+    if (strW <= 30)
+      return (<div style={{
+        textOverflow: "ellipsis",
+        overflow: "hidden",
+        whiteSpace: "nowrap"
+      }}>{text}</div>);
+
+    return (<Tooltip title={text}>
+      <div style={{
+        textOverflow: "ellipsis",
+        overflow: "hidden",
+        whiteSpace: "nowrap"
+      }}>{text}</div>
+    </Tooltip>);
+  }, [strW, text]);
 }
 
 const WorkflowTypeSelect: React.FC<WorkflowTypeSelectPropsI> = ({value, onChange}) => {
@@ -29,7 +60,7 @@ const WorkflowTypeSelect: React.FC<WorkflowTypeSelectPropsI> = ({value, onChange
         <CheckCard
           key={item.only_name}
           title={item.name}
-          description={item?.illustrate || '-'}
+          description={<CardTooltip text={item?.illustrate} />}
           value={item.only_name}
         />
       ))}
